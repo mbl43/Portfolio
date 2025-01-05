58fa64346d893fa1a6cd8b830c0ac3db4906db20
mangesh <mangeshlemte.a23@gmail.com>
2025-01-05 21:18:08 +0530

    style: react animated cursor,lucide react, developer icons

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,38 @@
-import { About, Contact, Experience, Home, Navbar, Portfolio, Social } from "./Components/index";
+import {
+  About,
+  Contact,
+  Experience,
+  Home,
+  Navbar,
+  Portfolio,
+  Social,
+} from "./Components/index";
 import "./App.css";
+import AnimatedCursor from "react-animated-cursor";
 function App() {
   return (
     <div className="">
       <Navbar />
       <Home />
-      <Portfolio/>
-      <Experience/>
+      <Portfolio />
+      <Experience />
       <Social />
-      <About/>
-      <Contact/>
+      <About />
+      <Contact />
+      <AnimatedCursor
+        innerSize={8}
+        outerSize={35}
+        innerScale={1}
+        outerScale={2}
+        outerAlpha={0}
+        hasBlendMode={true}
+        innerStyle={{
+          backgroundColor: "var(--cursor-color)",
+        }}
+        outerStyle={{
+          border: "3px solid var(--cursor-color)",
+        }}
+      />
     </div>
   );
 }
