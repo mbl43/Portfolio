d7492ce1e0cf2a02ef972a99850c68f52503c2cd
mangesh <mangeshlemte.a23@gmail.com>
2025-01-12 14:26:36 +0530

    style: add frosted navbar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -26,7 +26,7 @@ function Navbar() {
     },
   ];
   return (
-    <div className="flex z-50 justify-between items-center fixed bg-black w-full h-20 text-white md:justify-around ">
+    <div className="flex z-50 justify-between items-center bg-black w-full h-20 text-white md:justify-around sticky backdrop-filter backdrop-blur-sm bg-opacity-35 top-0">
       <div>
         <h1 className="text-3xl pl-2">
           <a href="/"
@@ -51,7 +51,7 @@ function Navbar() {
       {/* hamburger start*/}
       <div
         onClick={() => setnav(!nav)}
-        className="  pl-10 z-10 cursor-pointer pr-4 md:hidden"
+        className="pl-10 z-10 cursor-pointer pr-4 md:hidden"
       >
         {nav ? <FaTimes size={30} /> : <FaBars size={30} />}
       </div>
@@ -59,7 +59,7 @@ function Navbar() {
         <ul className="flex flex-col justify-center items-center absolute top-0 left-0 w-full h-screen bg-gradient-to-b from-black to-gray-800 text-gray ">
           {links.map(({ link, id }) => (
             <li
-              className="px-4  cursor-pointer hover:scale-125 duration-100 hover:text-cyan-300 capitalize py-4 text-3xl
+              className="px-4 cursor-pointer hover:scale-125 duration-100 hover:text-cyan-300 capitalize py-4 text-3xl
            "
               key={id}
             >
