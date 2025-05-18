4e041bdc97ea007b009912428ec0c25000df03b0
mangesh <mangeshlemte.a23@gmail.com>
2025-05-18 15:51:19 +0530

    Add experience calculation and pass it to Home and Experience components

--- src/App.jsx
+++ src/App.jsx
@@ -9,13 +9,34 @@ import {
 } from "./Components/index";
 import "./App.css";
 import AnimatedCursor from "react-animated-cursor";
+import { useEffect, useState } from "react";
 function App() {
+  const[experience,setExperience]=useState('2')
+   useEffect(() => {
+    const calculateExperience = () => {
+      const now = new Date();
+      const start = new Date("2022-10-18T00:00:00");
+      const diff = now - start;
+
+      const years = Math.floor(diff / (1000 * 60 * 60 * 24 * 365));
+      const remAfterYears = diff % (1000 * 60 * 60 * 24 * 365);
+      const months = Math.floor(remAfterYears / (1000 * 60 * 60 * 24 * 30));
+      const exper = `${years}.${months}`;
+
+      setExperience(exper);
+    };
+
+    calculateExperience(); // initial run
+    const intervalId = setInterval(calculateExperience, 1000);
+
+    return () => clearInterval(intervalId); // cleanup
+  }, []);
   return (
     <div className="">
       <Navbar />
-      <Home />
+      <Home experience={experience}/>
       <Portfolio />
-      <Experience />
+      <Experience experience={experience} />
       <Social />
       <About />
       <Contact />
